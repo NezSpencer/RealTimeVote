5c97c475c466f43ada15a69848df22bc42d6029c
Nnabueze Uhiara <eze@reach.africa>
2019-02-04 22:17:44 +0100

    clean up code

--- app/src/main/java/com/nezspencer/realtimevote/App.kt
+++ app/src/main/java/com/nezspencer/realtimevote/App.kt
@@ -1,14 +1,5 @@
 package com.nezspencer.realtimevote
 
 import android.app.Application
-import com.nezspencer.realtimevote.model.Election
 
-class App : Application() {
-companion object {
-    @JvmStatic lateinit var elections : ArrayList<Election>
-}
-    override fun onCreate() {
-        super.onCreate()
-        elections = ArrayList()
-    }
-}
+class App : Application()
